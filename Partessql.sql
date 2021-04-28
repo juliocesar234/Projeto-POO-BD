740c0d12075f1cb80046518f352b1168f59c3fc7
KayanBBarros <79804441+KayanBBarros@users.noreply.github.com>
2021-04-27 21:51:10 -0300

    Atualização 27/04/21

--- Partessql.sql
+++ Partessql.sql
@@ -38,3 +38,71 @@ create table possui (
 );
 
 
+
+SET enable_seqscan = OFF;
+
+EXPLAIN SELECT nome_exercício FROM exercicios WHERE id=100;
+CREATE INDEX index_exercicio ON exercicios USING hash (id);
+
+EXPLAIN SELECT  quantidade_assuntos FROM disciplina WHERE quantidade_assuntos >2;
+CREATE INDEX index_disciplina ON disciplina USING hash (quantidade_assuntos);
+
+EXPLAIN SELECT nome FROM assunto WHERE nome= 'Função';
+CREATE INDEX index_assunto ON assunto USING hash (nome);
+
+EXPLAIN SELECT nome_exercício FROM exercicios WHERE id>100;
+CREATE INDEX index_exercicio1 ON exercicios USING hash (id);
+
+EXPLAIN SELECT nome_exercício FROM exercicios WHERE id<100;
+CREATE INDEX index_exercicio2 ON exercicios USING hash (id);
+
+EXPLAIN SELECT nome FROM assunto WHERE nome = 'Equacão';
+CREATE INDEX index_assunto1 ON assunto USING hash (nome);
+
+/*edição de Kayan --->*/   
+EXPLAIN SELECT nome FROM assunto WHERE nome = 'Funcaoafim';
+CREATE INDEX index_assunto3 ON assunto USING hash (nome);
+
+EXPLAIN SELECT nome_exercício FROM exercicios WHERE id=10;
+CREATE INDEX index_exercicio4 ON exercicios USING hash (id);
+
+EXPLAIN SELECT  quantidade_assuntos FROM disciplina WHERE quantidade_assuntos =14;
+CREATE INDEX index_disciplina1 ON disciplina USING hash (quantidade_assuntos);
+
+EXPLAIN SELECT id FROM assunto WHERE id >=24;
+CREATE INDEX index_assunto3 ON assunto USING hash (id);
+
+EXPLAIN SELECT id FROM exercicios WHERE id >4;
+CREATE INDEX index_exercicio5 ON assunto USING hash(id);
+
+/* Júlio */
+EXPLAIN SELECT nível FROM exercicios WHERE nível = 'Fácil' ;
+CREATE INDEX exerciciosnivel ON exercicios USING hash(nível);
+
+EXPLAIN SELECT area_do_conhecimento FROM disciplina WHERE area_do_conhecimento = 'Ciências humanas';
+CREATE INDEX disciplinaarea ON disciplina USING hash (area_do_conhecimento);
+
+EXPLAIN SELECT nível FROM exercicios WHERE nível = 'Difícil' ;
+CREATE INDEX exerciciosnivel1 ON exercicios USING hash(nível);
+
+EXPLAIN SELECT nível FROM exercicios WHERE nível = 'Médio' ;
+CREATE INDEX exerciciosnivel2 ON exercicios USING hash(nível);
+
+EXPLAIN SELECT area_do_conhecimento FROM disciplina WHERE area_do_conhecimento = 'Medicina';
+CREATE INDEX disciplinaarea1 ON disciplina USING hash (area_do_conhecimento);
+
+/* JM */
+EXPLAIN SELECT area_do_conhecimento FROM disciplina WHERE area_do_conhecimento = 'Direito';
+CREATE INDEX area_disciplina1 ON disciplina USING hash (area_do_conhecimento);
+
+EXPLAIN SELECT area_do_conhecimento FROM disciplina WHERE area_do_conhecimento = 'TI';
+CREATE INDEX area_disciplina2 ON disciplina USING hash (area_do_conhecimento);
+
+EXPLAIN SELECT nome FROM assunto WHERE nome = 'Crise de 1929';
+CREATE INDEX nome_assunto1 ON assunto USING hash (nome);
+
+EXPLAIN SELECT nome FROM assunto WHERE nome= 'Genética';
+CREATE INDEX nome_assunto2 ON assunto USING hash (nome);
+
+EXPLAIN SELECT texto_comando FROM exercicios WHERE texto_comando = 'De quantas maneiras ele poderá escalar seu time?'; /* id 90 */
+CREATE INDEX texto_comando ON exercicios USING hash (texto_comando); 
